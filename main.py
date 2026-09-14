a589123244d6011484a4ca0589231fb647705836
Aleks "F1SZTE <u182_mocale_waw@technischools.com>
2026-09-14 14:56:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-.
+import sqlite3
+
+con = sqlite3.connect("library.db")
+cur = con.cursor()
+
+cur.execute("""CREATE TABLE IF NOT EXISTS users(
+            id INTIGER PRIMARY KEY AUTOINCREMENT,
+            name VARCHAR(50) NOT NULL,
+            surname VARCHAR(50) NOT NULL,
+            email VARCHAR(50) NOT NULL UNIQUE,
+            phone VARCHAR(12) NOT NULL)""")
+
+cur.execute("""CREATE TABLE IF NOT EXISTS transactions
+            (
+            id INTIGER PRIMARY KEY AUTOINCREMENT,
+            amount DECIMAL(10,2) NOT NULL,
+            userfrom_id INTIGER NOT NULL.
+            userto_id INTIGER NOT NULL,
+            date VARCHAR(20) NOT NULL,
+            FORGERY KEY (userfrim_id) REFERENCES users
+            )
+            """)
